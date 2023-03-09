ce296b3b671eb0e4c5cb52c0a43446dcae0b5666
seekparadox <wallermichael14@gmail.com>
2023-03-09 17:33:43 -0500

    "Added feature product section to home page"

--- Sales Website/src/Components/ProductCard.jsx
+++ Sales Website/src/Components/ProductCard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const ProductCard = (prop) => {
-    return (
-        <div></div>
-    )
-}
+const ProductCard = (props) => {
+  return (
+    <div className="ProductCard">
+      <div className="card-image">
+        <img src={props.image} alt="" srcset="" />
+      </div>
+      <span className="product-card-text product-card-description">{props.description}</span><br />
+      <span className="product-card-text product-card-cost">{props.cost}</span><br />
+
+    </div>
+  );
+};
 
 export default ProductCard;
